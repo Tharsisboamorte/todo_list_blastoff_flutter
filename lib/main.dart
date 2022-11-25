70e941e3c7a3481b28d0f051176ad9d1ce792a55
Lamin Lumumba <tharsis.queiroz@toodoo.com.br>
2022-11-25 10:16:05 -0300

    Todo list challenge class

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:todo_list_flutter_challenge/todo_list_page.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: TodoListPage(),
+  ));
+}
